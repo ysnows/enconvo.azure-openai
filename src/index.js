d1ab251eb912a3040cb2a325ed512b0225f88d0b
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-07-25 22:07:02 +0800

    1. optimize sdk

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const {HumanMessage, AIMessage, SystemMessage} = require("langchain/schema");
+const {res, req} = require("enconvo/bridge");
+const {CallbackManager} = require("langchain/callbacks");
+const {ChatOpenAI} = require("enconvo/llm/openai");
+
+
+(async () => {
+    const {context, options} = req.body();
+    console.log(`process begin...${JSON.stringify(context)}`)
+
+    const chat = new ChatOpenAI({
+        streaming: true,
+        temperature: +options.temperature,
+        azureOpenAIApiKey: options.apiKey,
+        azureOpenAIApiVersion: options.apiVersion,
+        azureOpenAIApiDeploymentName: options.model,
+        azureOpenAIApiInstanceName: options.instanceName,
+        verbose: true
+    });
+
+    const messages = context.messages.map((item) => {
+        if (item.role === "assistant") {
+            return new AIMessage(item.content)
+        } else if (item.role === "system") {
+            return new SystemMessage(item.content)
+        }
+        return new HumanMessage(item.content)
+    });
+
+    await chat.call(messages, {}, CallbackManager.fromHandlers({
+        handleLLMNewToken(token, idx, runId, parentRunId, tags) {
+            res.write(token);
+        }
+    }));
+    res.end()
+})();
